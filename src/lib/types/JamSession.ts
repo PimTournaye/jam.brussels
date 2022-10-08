d664706c95ec14abac88af33d19926853b84bdb7
PimTournaye <pim.tournaye@student.ehb.be>
2022-10-08 17:43:57 +0200

    db client-side constuction

---
+++ src/lib/types/JamSession.ts
@@ -0,0 +1,11 @@
+export type JamSession = {
+  name: string;
+  date: Date; 
+  location: string;
+  time: string;
+  openingBand: string;
+  type: string;
+  description: string;
+  image: string;
+};
+    
