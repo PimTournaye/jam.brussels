c4cb475ed8d95778940b5455edb03756098d6304
PimTournaye <pim.tournaye@student.ehb.be>
2022-11-13 13:56:13 +0100

    added id's to testData

--- src/lib/utils/testCards.ts
+++ src/lib/utils/testCards.ts
@@ -1,18 +1,18 @@
-// Generate some test events
 export const jamSessions = [
   {
-    name: 'Jam Session 1',
+    title: 'Jam Session 1',
     date: new Date(),
-    location: 'Location 1',
+    location: 'Bar Moun',
     time: '12:00',
     organizerId: 1,
     openingBand: 'Opening Band 1',
     type: 'Type 1',
     description: 'Description 1',
     image: 'Image 1', 
+    id: 1
   },
   { 
-    name: 'Jam Session 2',
+    title: 'Jam Session 2',
     date: new Date().getDate() + 1,
     location: 'Location 2',
     time: '12:00',
@@ -21,9 +21,10 @@ export const jamSessions = [
     type: 'Type 2',
     description: 'Description 2',
     image: 'Image 2',
+    id: 2
   },
   {
-    name: 'Jam Session 3',
+    title: 'Jam Session 3',
     date: new Date().getDate() + 2,
     location: 'Location 3',
     time: '12:00',
@@ -32,9 +33,10 @@ export const jamSessions = [
     type: 'Type 3',
     description: 'Description 3',
     image: 'Image 3',
+    id: 3
   },
   {
-    name: 'Jam Session 4',
+    title: 'Jam Session 4',
     date: new Date().getDate() - 3,
     location: 'Location 4',
     time: '12:00',
@@ -43,5 +45,6 @@ export const jamSessions = [
     type: 'Type 4',
     description: 'Description 4',
     image: 'Image 4',
+    id: 4
   },
 ];
