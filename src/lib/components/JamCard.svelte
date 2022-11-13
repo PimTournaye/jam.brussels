57023443d85b7a041f19e50842a56333a70687c6
PimTournaye <pim.tournaye@student.ehb.be>
2022-11-13 13:57:30 +0100

    fixed styling, plus naming

--- src/lib/components/JamCard.svelte
+++ src/lib/components/JamCard.svelte
@@ -1,37 +1,44 @@
 <script lang="ts">
 	import { Icon } from '@steeze-ui/svelte-icon';
-  import { Clock, Calendar, MapPin, MusicalNote } from '@steeze-ui/heroicons';
+	import { Clock, Calendar, MapPin, MusicalNote } from '@steeze-ui/heroicons';
+	import { formatShortDate } from '$lib/utils/DateFormatting';
 
-	let image: string;
-	let title: string;
-	let date: string;
-	let time: string;
-	let location: string;
-	let opener: string;
-
-  let color: string;
-
+	export let image: string;
+	export let title: string;
+	export let date: Date;
+	export let time: string;
+	export let location: string;
+	export let openingBand: string;
+      export let id: string;
 </script>
 
-<div class="rounded-lg max-h-100">
-	<img class="h-60%" src={image} alt="Picture of jam at {location}" />\
-	<h1 class="text-2xl font-bold">{title}</h1>
-	<div class="p-4 flex">
-		<div class="w-1/2">
-      <h2 class="text-sm text-[#CACACA] font-regular mb-2">
-            <Icon src={MapPin} theme="solid" class="inline-block w-4 h-4" />
-            {location}</h2>
-      <h2 class="text-sm text-[#CACACA] font-regular">
-            <Icon src={Calendar} theme="solid" class="inline-block w-4 h-4" />
-            {date}</h2>
-    </div>
-		<div class="w-1/2">
-      <h2 class="text-sm text-[#CACACA] font-regular mb-2">
-            <Icon src={MusicalNote} theme="solid" class="inline-block w-4 h-4" />
-            {opener}</h2>
-      <h2 class="text-sm text-[#CACACA] font-regular">
-            <Icon src={Clock} theme="solid" class="inline-block w-4 h-4" />
-            {time}</h2>
-    </div>
-	</div>
-</div>
+<a href="/jams/{id}">
+      <div class="rounded-lg max-h-100 w-full mt-2 mx-1">
+            <img class="h-48" src={image} alt="Picture of jam at {location}" />
+            <h1 class="text-2xl font-bold bg-tulip-tree px-4 py-2 text-log-cabin">{title}</h1>
+      
+            <div class="p-4 flex rounded-b-lg bg-cararra">
+                  <div class="w-1/2">
+                        <h2 class="text-sm text-log-cabin font-regular mb-2">
+                              <Icon src={MapPin} theme="solid" class="inline-block w-4 h-4 mr-1" />
+                              {location}
+                        </h2>
+                        <h2 class="text-sm text-log-cabin font-regular">
+                              <Icon src={Calendar} theme="solid" class="inline-block w-4 h-4 mr-1" />
+                              {formatShortDate(date)}
+                              <!-- {date} -->
+                        </h2>
+                  </div>
+                  <div class="w-1/2">
+                        <h2 class="text-sm text-log-cabin font-regular mb-2">
+                              <Icon src={MusicalNote} theme="solid" class="inline-block w-4 h-4 mr-1" />
+                              {openingBand}
+                        </h2>
+                        <h2 class="text-sm text-log-cabin font-regular">
+                              <Icon src={Clock} theme="solid" class="inline-block w-4 h-4 mr-1" />
+                              {time}
+                        </h2>
+                  </div>
+            </div>
+      </div>
+</a>
