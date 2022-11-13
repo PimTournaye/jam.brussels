93c037f4dff3d2159df396e43db343f16852777b
PimTournaye <pim.tournaye@student.ehb.be>
2022-11-13 19:34:23 +0100

    fixes

--- src/lib/components/JamCard.svelte
+++ src/lib/components/JamCard.svelte
@@ -9,36 +9,35 @@
 	export let time: string;
 	export let location: string;
 	export let openingBand: string;
-      export let id: string;
+	export let id: string;
 </script>
 
 <a href="/jams/{id}">
-      <div class="rounded-lg max-h-100 w-full mt-2 mx-1">
-            <img class="h-48" src={image} alt="Picture of jam at {location}" />
-            <h1 class="text-2xl font-bold bg-tulip-tree px-4 py-2 text-log-cabin">{title}</h1>
-      
-            <div class="p-4 flex rounded-b-lg bg-cararra">
-                  <div class="w-1/2">
-                        <h2 class="text-sm text-log-cabin font-regular mb-2">
-                              <Icon src={MapPin} theme="solid" class="inline-block w-4 h-4 mr-1" />
-                              {location}
-                        </h2>
-                        <h2 class="text-sm text-log-cabin font-regular">
-                              <Icon src={Calendar} theme="solid" class="inline-block w-4 h-4 mr-1" />
-                              {formatShortDate(date)}
-                              <!-- {date} -->
-                        </h2>
-                  </div>
-                  <div class="w-1/2">
-                        <h2 class="text-sm text-log-cabin font-regular mb-2">
-                              <Icon src={MusicalNote} theme="solid" class="inline-block w-4 h-4 mr-1" />
-                              {openingBand}
-                        </h2>
-                        <h2 class="text-sm text-log-cabin font-regular">
-                              <Icon src={Clock} theme="solid" class="inline-block w-4 h-4 mr-1" />
-                              {time}
-                        </h2>
-                  </div>
-            </div>
-      </div>
+	<div class="rounded-lg max-h-100 w-full mt-2 mx-1">
+		<img class="h-48" src={image} alt="Picture of jam at {location}" />
+		<h1 class="text-2xl font-bold bg-tulip-tree px-4 py-2 text-log-cabin">{title}</h1>
+
+		<div class="p-4 flex rounded-b-lg bg-cararra">
+			<div class="w-1/2">
+				<h2 class="text-sm text-log-cabin font-regular mb-2">
+					<Icon src={MapPin} theme="solid" class="inline-block w-4 h-4 mr-1" />
+					{location}
+				</h2>
+				<h2 class="text-sm text-log-cabin font-regular">
+					<Icon src={Calendar} theme="solid" class="inline-block w-4 h-4 mr-1" />
+					{formatShortDate(date)}
+				</h2>
+			</div>
+			<div class="w-1/2">
+				<h2 class="text-sm text-log-cabin font-regular mb-2">
+					<Icon src={MusicalNote} theme="solid" class="inline-block w-4 h-4 mr-1" />
+					{openingBand}
+				</h2>
+				<h2 class="text-sm text-log-cabin font-regular">
+					<Icon src={Clock} theme="solid" class="inline-block w-4 h-4 mr-1" />
+					{time}
+				</h2>
+			</div>
+		</div>
+	</div>
 </a>
