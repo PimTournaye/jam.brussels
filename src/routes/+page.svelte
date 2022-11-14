5d27f87763233287ecaa9313b88e7fecdaf28f46
PimTournaye <pim.tournaye@student.ehb.be>
2022-11-14 20:12:18 +0100

    added placeholder to homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,32 +4,42 @@
 	export let data: any;
 	let { todayJams, upcomingJams } = data;
 
-	console.log(todayJams, upcomingJams);
+	let todayColor = 'bg-tulip-tree';
+
 </script>
+
+<svelte:head>
+	<title>Home</title>
+</svelte:head>
 
 <div class="mt-8 mx-4">
 	<h1 class="text-4xl font-bold">Welcome back!</h1>
-	<h2 class="text-lg text-willow-brook font-bold">Here's what's happening soon</h2>
+	<h2 class="text-lg text-cararra-900 font-bold">Here's what's happening soon</h2>
 </div>
 
 <section class="mt-8 mb-8">
 	<div>
 		<h1 class="text-bold text-2xl mx-4">Today</h1>
-		{#if todayJams}
-			{#each todayJams as { image, title, date, time, location, openingBand, id }}
-			<JamCard {image} {title} {date} {time} {location} {openingBand} {id} />
-			{/each}
+		{#if !todayJams || todayJams.length === 0}
+		<div class="flex flex-col justify-center mx-auto my-4
+		w-full h-60
+		border-4 border-cararra-900 rounded-xl">
+			<h1 class="text-bold text-2xl mx-auto mb-8">No events today!</h1>
+			<img src="/music_note_white_24dp.svg" class="w-16 h-16 mx-auto text-cararra-900" alt="icon of a music note"/>
+		</div>
 		{:else}
-			<h1 class="text-bold text-2xl">No events today</h1>
+			{#each todayJams as { image, title, date, startTime, endTime, location, openingBand, id }}
+			<JamCard {image} {title} {date} {startTime} {endTime} {location} {openingBand} color={'tulip-gold'}{id} />
+			{/each}
 		{/if}
 	</div>
 
 	<div class="mt-8">
 		<h1 class="text-bold text-2xl mx-4">Upcoming</h1>
-		
+
 		{#if upcomingJams}
-			{#each upcomingJams as { image, title, date, time, location, openingBand, id }}
-			<JamCard {image} {title} {date} {time} {location} {openingBand} {id} />
+			{#each upcomingJams as { image, title, date, startTime, endTime, location, openingBand, id }}
+			<JamCard {image} {title} {date} {startTime} {endTime} {location} {openingBand} color={'cinnabar'} {id} />
 			{/each}
 		{:else}
 			<h1 class="text-bold text-2xl">No upcoming events</h1>
