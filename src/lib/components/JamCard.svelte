d10df560190331711482f0054eabd24655d5f7fa
PimTournaye <pim.tournaye@student.ehb.be>
2022-11-14 15:48:23 +0100

    added new variables, temp unsplash pictures

--- src/lib/components/JamCard.svelte
+++ src/lib/components/JamCard.svelte
@@ -6,15 +6,16 @@
 	export let image: string;
 	export let title: string;
 	export let date: Date;
-	export let time: string;
+	export let startTime: string;
+	export let endTime: string;
 	export let location: string;
 	export let openingBand: string;
 	export let id: string;
 </script>
 
 <a href="/jams/{id}">
-	<div class="rounded-lg max-h-100 w-full mt-2 mx-1">
-		<img class="h-48" src={image} alt="Picture of jam at {location}" />
+	<div class="rounded-lg max-h-100 w-full mt-12 mx-1">
+		<img class="" src="https://source.unsplash.com/1600x900/?music" alt="Picture of jam at {location}" />
 		<h1 class="text-2xl font-bold bg-tulip-tree px-4 py-2 text-log-cabin">{title}</h1>
 
 		<div class="p-4 flex rounded-b-lg bg-cararra">
@@ -35,7 +36,7 @@
 				</h2>
 				<h2 class="text-sm text-log-cabin font-regular">
 					<Icon src={Clock} theme="solid" class="inline-block w-4 h-4 mr-1" />
-					{time}
+					{startTime} - {endTime}
 				</h2>
 			</div>
 		</div>
