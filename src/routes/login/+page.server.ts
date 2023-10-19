1ee27a8ad9324443466cff356d734d983be5bda5
PimTournaye <pimtournaye1@gmail.com>
2023-10-19 18:34:14 +0200

    ACCOUNT: login constructed

--- src/routes/login/+page.server.ts
+++ src/routes/login/+page.server.ts
@@ -7,6 +7,7 @@ import { auth } from "$lib/server/lucia";
 import { z } from 'zod';
 import { superValidate, message } from 'sveltekit-superforms/server';
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
+import { LuciaError } from 'lucia';
 
 const loginSchema = z.object({
   email: z.string().email(),
@@ -23,9 +24,9 @@ const registerSchema = z.object({
 export const load: PageServerLoad = async ({ locals }) => {
   // If the user is already logged in, redirect them to the home page
   const session = await locals.auth.validate();
-	if (session) throw redirect(302, "/"); 
+  if (session) throw redirect(302, "/");
 
-	// Different schemas, so no id required.
+  // Different schemas, so no id required.
   const loginForm = await superValidate(loginSchema);
   const registerForm = await superValidate(registerSchema);
 
@@ -34,31 +35,49 @@ export const load: PageServerLoad = async ({ locals }) => {
 };
 
 export const actions: Actions = {
-	login: async ({ request }) => {
-    const registerForm = await superValidate(request, registerSchema);
+  login: async ({ request, locals }) => {
+    // Validate the form data
+    const loginForm = await superValidate(request, registerSchema);
+    if (!loginForm.valid) return message(loginForm, 'Invalid form data', { status: 400 });
 
-    if (!registerForm.valid) return fail(400, { registerForm });
-
-    // TODO: Register user
-    return message(registerForm, 'Register form submitted');
+    try {
+      const key = await auth.useKey(
+        'email',
+        loginForm.data.email.toLowerCase(),
+        loginForm.data.password,
+      );
+      const session = await auth.createSession({
+        userId: key.userId,
+        attributes: {},
+      });
+      locals.auth.setSession(session); // set session cookie
+      return message(loginForm, 'Register form submitted');
+    } catch (error) {
+      if (error instanceof LuciaError &&
+        (error.message === "AUTH_INVALID_KEY_ID" || error.message === "AUTH_INVALID_PASSWORD")) {
+        // user does not exist or invalid password
+        return message(loginForm, "Invalid email or password", { status: 401 });
+      }
+    }
+    throw redirect(302, "/");
   },
 
   register: async ({ request, locals }) => {
-		console.log("login action");
-		// Validate the form data
-		const loginForm = await superValidate(request, loginSchema);
-    if (!loginForm.valid) return fail(400, { loginForm });
+    console.log("login action");
+    // Validate the form data
+    const registerForm = await superValidate(request, registerSchema);
+    if (!registerForm.valid) return fail(400, { registerForm });
 
     try {
       // Create the user
       const user = await auth.createUser({
         key: {
           providerId: 'email',
-          providerUserId: loginForm.data.email.toLowerCase(),
-          password: loginForm.data.password,
+          providerUserId: registerForm.data.email.toLowerCase(),
+          password: registerForm.data.password,
         },
         attributes: {
-          email: loginForm.data.email.toLowerCase(),
+          email: registerForm.data.email.toLowerCase(),
         }
       })
       // Create the session so the user is logged in
@@ -67,15 +86,15 @@ export const actions: Actions = {
         attributes: {},
       });
       // Store the session in the session storage
-      locals.auth.setSession(session)
+      locals.auth.setSession(session);
     } catch (error) {
       // Check for database errors
       if (error instanceof PrismaClientKnownRequestError) {
-        return message(loginForm, `Error: ${error.code}, ${error.meta} ${error.message}`, { status: 409 });
+        return message(registerForm, `Error: ${error.code}, ${error.meta} ${error.message}`, { status: 409 });
       }
       // Catch all other errors
-      return message(loginForm, 'Something went wrong creating the account', { status: 500 });
+      return message(registerForm, 'Something went wrong creating the account', { status: 500 });
     }
-    return message(loginForm, 'Login form submitted');
-	},
+    throw redirect(302, "/");
+  },
 };
